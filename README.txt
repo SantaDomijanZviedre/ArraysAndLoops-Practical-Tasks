342dbda47a3ecce12035653202de20f81dc6508c
Santa Domijan Zviedre <77302444+SantaDomijanZviedre@users.noreply.github.com>
2021-02-02 00:52:26 +0100

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,4 @@
+Practical Tasks
+Arrays and Loops
+
+Assignment 1 to 3. // 01.02.2021.
---
+++ src/com/company/Assignment2Loops.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Assignment2Loops {
+
+   /* public static void main(String[] args) {
+        negativeElements();
+        //forEachLoop();
+        //whileLoop();
+        //doWhileLoop();
+    }
+
+    public static void negativeElements() {
+
+        int[] santasArray2 = new int[]{10, 4, -4, 7, 0, 9, 1, 3, 7, -4};
+
+        int i;
+
+        while (i >= 0) {
+            i++;
+
+            if (i < 0) {
+            System.out.println("Negative number: " + i);
+            break;
+            }
+        }
+    }*/
+}
