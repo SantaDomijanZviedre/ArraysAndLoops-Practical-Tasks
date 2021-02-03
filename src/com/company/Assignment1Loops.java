22983fc1081652fd63b36f858c8cef5645372d56
Santa Domijana-Zviedre <balticadriatic@gmail.com>
2021-02-03 15:25:47 +0100

    Assignment 1 Loops, Assignment 2 Loops

---
+++ src/com/company/Assignment1Loops.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Assignment1Loops {
+
+    public static void main(String[] args) {
+
+        int[] santasArray = new int[]{5, 3, 7, 6, 2, 8};
+        forLoop(santasArray);
+        forEachLoop(santasArray);
+        whileLoop(santasArray);
+        doWhileLoop(santasArray);
+    }
+
+    public static void forLoop(int[] santasArray) {
+
+        for (int i = 0; i < santasArray.length; i++) {
+            System.out.println("FOR Loop: " + i + "[" + santasArray[i] + "]");
+        }
+    }
+
+    public static void forEachLoop(int[] santasArray1) {
+
+        for (int tempVar : santasArray1) {
+            System.out.println("For EACH Loop: " + tempVar);
+        }
+        //santasArray1[1]=42; experiment
+    }
+
+    public static void whileLoop(int[] santasArray2) {
+
+        int i = 0;
+        while(i < santasArray2.length) {
+            System.out.println("WHILE Loop: " + santasArray2[i]);
+            i++;
+        }
+    }
+
+    public static void doWhileLoop(int[] santasArray3) {
+
+        int i = 0;
+        do {
+            System.out.println("DO WHILE Loop: " + santasArray3[i]);
+            i++;
+        } while(i < santasArray3.length);
+    }
+}
--- src/com/company/Assignment2Loops.java
+++ src/com/company/Assignment2Loops.java
@@ -1,27 +1,102 @@
 package com.company;
 
+import java.util.Enumeration;
+
 public class Assignment2Loops {
 
-   /* public static void main(String[] args) {
-        negativeElements();
-        //forEachLoop();
-        //whileLoop();
-        //doWhileLoop();
+    public static void main(String[] args) {
+
+        int[] arrayTenElements = new int[]{10, 4, -4, 7, 0, 9, 1, 3, 7, -5};
+
+        negativeElements(arrayTenElements);
+        oddElements(arrayTenElements);
+        countOddElements(arrayTenElements);
+        samValueElements(arrayTenElements);
+        eachSecondElement(arrayTenElements);
+        averageValueOfArray(arrayTenElements);
+        smallerThanAverage(arrayTenElements);
     }
 
-    public static void negativeElements() {
+    public static void negativeElements(int[] arrayTenElements1) {
 
-        int[] santasArray2 = new int[]{10, 4, -4, 7, 0, 9, 1, 3, 7, -4};
+        System.out.println("Negative elements of array: ");
+        for (int num : arrayTenElements1) {
+            if (num < 0)
+                System.out.println(num);
+        }
+    }
 
-        int i;
+    public static void oddElements(int[] arrayTenElements2) {
 
-        while (i >= 0) {
-            i++;
+        System.out.println("Odd elements of array: ");
+        for (int num : arrayTenElements2) {
+            if (num % 2 != 0)
+                System.out.println(num);
+        }
+    }
 
-            if (i < 0) {
-            System.out.println("Negative number: " + i);
-            break;
+    public static void countOddElements(int[] arrayTenElements3) {
+
+        System.out.println("Count of Odd elements of array: ");
+
+        int countOdd = 0;
+        for (int num : arrayTenElements3) {
+            //countOdd += Math.abs(num % 2);
+            if (num % 2 != 0) {
+                countOdd++;
             }
         }
-    }*/
+        System.out.println(countOdd);
+    }
+
+    public static void samValueElements(int[] arrayTenElements4) {
+
+        int i;
+        int j;
+        for(i = 0; i < arrayTenElements4.length; i++) {
+
+            for(j = i+1; j < arrayTenElements4.length; j++) {
+                if(arrayTenElements4[i]==arrayTenElements4[j])
+                {
+                    System.out.println("There are the following elements with same value: " + arrayTenElements4[i]);
+                    //return; (if need to stop when the first match is found)
+                }
+            }
+        }
+    }
+
+    public static void eachSecondElement(int[] arrayTenElements5) {
+
+        System.out.println("Each second element of array: ");
+        for (int i = 0; i < arrayTenElements5.length; i = i + 2) {
+            System.out.println(arrayTenElements5[i]);
+        }
+    }
+
+    public static double averageValueOfArray(int[] arrayTenElements6) {
+
+        int sumAll = 0;
+        int i;
+        for (i = 0; i < arrayTenElements6.length; i++) {
+            sumAll = sumAll + arrayTenElements6[i];
+        }
+        //System.out.println("Average value of array is: " + sumAll/arrayTenElements6.length);
+        return ((double) sumAll) /arrayTenElements6.length; // force division to double
+
+    }
+
+    public static void smallerThanAverage(int[] arrayTenElements7) {
+
+        int countSmallerAvg = 0;
+        int i;
+        double avg = averageValueOfArray(arrayTenElements7); //
+        for (i = 0; i < arrayTenElements7.length; i++) {
+
+            if (arrayTenElements7[i] < avg) {
+                countSmallerAvg++;
+            }
+        }
+        System.out.println("There are " + countSmallerAvg + " elements smaller than average.");
+    }
 }
+
