ab66d6f1a0d87e4827f905b72fb708dac1811122
Santa Domijana-Zviedre <balticadriatic@gmail.com>
2021-02-15 12:31:04 +0100

    Assign 2 LoopsXX run Methods in Loop

--- src/com/company/Assignment2LoopsXX.java
+++ src/com/company/Assignment2LoopsXX.java
@@ -2,91 +2,44 @@ package com.company;
 
 public class Assignment2LoopsXX {
 
-    public static void main(String[] args)
-
-    {
+    public static void main(String[] args) {
         int[] arrayTenElements = new int[]{10, 4, -4, 7, 0, 9, 1, 3, 7, -5};
         System.out.println(analyseArray(arrayTenElements));
     }
 
-    public static boolean analyseArray(int[] arrayTenElements)
-    {
-        for (int num : arrayTenElements)
-        {
-           negativeElements(num);
-           oddElements(num);
-           countOddElements(num);
+    //this is good for exercise how to check Array with several methods at once
+    // but it does not function as original task
+
+    public static int analyseArray(int[] arrayTenElements) {
+        int oddCounter = 0;
+        for (int num : arrayTenElements) {
+            negativeElements(num);          // does not print out all negative elements, just the first one
+            oddElements(num); //oddCounter +=
+            countOddElements(num); // does not print out countOdd
         }
-        return true;
+        return oddCounter++; // asks for int Method and does not allow void
+        //System.out.println(oddCounter); // asks for return or requests int
     }
 
     public static void negativeElements(int num) {
 
-            if (num < 0)
+        if (num < 0)
             System.out.println(num);
     }
 
     public static void oddElements(int num) {
 
-            if (num % 2 != 0)
-                System.out.println(num);
+        if (num % 2 != 0)
+            System.out.println(num);
     }
 
-    public static int countOddElements(int num) {
-
-        int countOdd = 0;
-            if (num % 2 != 0) {
-                countOdd++;
-            }
-        return countOdd;
-    }
-
-   /* public static void sameValueElements(int[] arrayTen) {
-
-        int i;
-        //for(i = 0; i < arrayTen.length; i++) {
-            int j;
-          //  for(j = i+1; j < arrayTen.length; j++) {
-                if(arrayTen[i]==arrayTen[j])
-                {
-                    System.out.println("There are the following elements with same value: " + arrayTen[i]);
-                    //return; (if need to stop when the first match is found)
-                }
-            }
+    public static int countOddElements(int num) { // would make sense if this would be used as soon
+                                                // as oddElement is found, using oddElements Method
+        //int countOdd = 0;
+        if (num % 2 != 0) {
+            //      countOdd++;
+            return 1;
         }
+        return 0;
     }
-
-    public static void eachSecondElement(int[] arrayTen) {
-
-        System.out.println("Each second element of array: ");
-        for (int i = 0; i < arrayTen.length; i = i + 2) {
-            System.out.println(arrayTen[i]);
-        }
-    }
-
-    public static double averageValueOfArray(int[] arrayTen) {
-
-        int sumAll = 0;
-        int i;
-        for (i = 0; i < arrayTen.length; i++) {
-            sumAll = sumAll + arrayTen[i];
-        }
-        //System.out.println("Average value of array is: " + sumAll/arrayTenElements6.length);
-        return ((double) sumAll) /arrayTen.length; // force division to double
-
-    }
-
-    public static void smallerThanAverage(int[] arrayTen) {
-
-        int countSmallerAvg = 0;
-        int i;
-        double avg = averageValueOfArray(arrayTen); //
-        for (i = 0; i < arrayTen.length; i++) {
-
-            if (arrayTen[i] < avg) {
-                countSmallerAvg++;
-            }
-        }
-        System.out.println("There are " + countSmallerAvg + " elements smaller than average.");
-    }*/
 }
